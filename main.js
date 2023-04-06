6906286d68dfe8e1d0d19a12c36a308ba58ddc8c
Jackson Dahl <jacksondahl@Alexa.local>
2023-04-06 18:05:55 -0400

    Done w/ project

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ class DisplayInformation {
      */
     // from ip location
     this.country = data.country_name;
+    this.city = data.city;
     this.capital = data.country_capital;
     this.longitude = data.longitude;
     this.latitude = data.latitude;
@@ -58,10 +59,34 @@ getCurrentTab();
 const ipGeolocation_API_key = 'e025e69f35024a1e811f198d05431291';
 let myIp = {'https://www.facebook.com/': '157.240.241.35',
             'https://www.google.com/': '142.250.81.238',
+            'https://alaska.gov/': '158.145.65.37',
             'https://www.novinky.cz/': '77.75.78.151',
             'https://www.baidu.com/': '110.242.68.66',
-            'https://shopee.vn/': '103.117.240.34'};
+            'https://shopee.vn/': '103.117.240.34',
+            'https://www.hasil.gov.my/':'113.11.238.133',
+            'https://www.kpay.com.kw/portal/':'195.137.190.29'};
 
+const common_weather = {
+  'clear sky': 'https://openweathermap.org/img/wn/01d@4x.png',
+  'few clouds': 'https://openweathermap.org/img/wn/02d@4x.png',
+  'scattered clouds': 'https://openweathermap.org/img/wn/03d@4x.png',
+  'broken clouds': 'https://openweathermap.org/img/wn/04d@4x.png',
+  'shower rain':'https://openweathermap.org/img/wn/09d@4x.png',
+  'rain': 'https://openweathermap.org/img/wn/10d@4x.png',
+  'thunderstorm': 'https://openweathermap.org/img/wn/11d@4x.png',
+  'snow':'https://openweathermap.org/img/wn/13d@4x.png',
+  'mist':'https://openweathermap.org/img/wn/50d@4x.png',
+  'overcast clouds':'https://openweathermap.org/img/wn/04d@4x.png'
+}
+
+const fun_facts = {
+  'China': 'Toilet paper was invented in China',
+  'United States': 'More people live in New York City than in 40 out of 50 of the states',
+  'Vietnam': 'Almost every household in Vietnam has a motorcycle',
+  'Czech Republic': 'The Czech Republic has the most castles out of every country in Europe.',
+  'Malaysia': 'The worlds largest cave chamber is in Malaysia',
+  'Kuwait': 'The Kuwait Dinar is the most valuable currency in the world',
+}
 
 //logJSONData('https://www.google.com/');
 
@@ -93,8 +118,7 @@ function buildHTML(location_obj, web_ip) {
   let countryInput = document.querySelector('#facts > :first-child');
   countryInput.textContent = location_obj.country;
   let capitalInput = document.querySelector('#facts > :nth-child(3)');
-  console.log(capitalInput)
-  capitalInput.textContent = location_obj.capital;
+  capitalInput.textContent = location_obj.city;
   let flagInput = document.querySelector('#facts > :nth-child(2)');
   let flagImage = document.createElement('img');
   flagImage.setAttribute('src', location_obj.flag_path);
@@ -104,7 +128,38 @@ function buildHTML(location_obj, web_ip) {
   longInput.textContent = `longitude: ${location_obj.longitude}`;
   let latInput = document.querySelector('#facts > :nth-child(5)');
   latInput.textContent = `latitude: ${location_obj.latitude}`;
-}
+  let funFact = document.querySelector('#facts > :nth-child(6)')
+  funFact.textContent = `funFact: ${fun_facts[location_obj.country]}`;
+  //populate weather
+  // this.temperature = data.main.temp;
+  // this.wind_speed = Math.floor(data.wind.speed);
+  // this.description = data.weather[0].description;
+  // //this.percipitation = data.rain['1hr'];
+  // this.cloudcover = data.clouds.all;
+  // this.visibility = data.visibility;
+  let temperatureInput = document.querySelector('#Weather > :first-child');
+  temperatureInput.textContent = `Temperature: ${Math.floor(location_obj.temperature - 273.15)}C`;
+  let wind_speedInput = document.querySelector('#Weather > :nth-child(2)');
+  console.log(location_obj.wind_speed);
+  wind_speedInput.textContent = `Wind Speed: ${location_obj.wind_speed}km/h`;
+  let descriptionInput = document.querySelector('#Weather > :nth-child(3)');
+  descriptionInput.textContent = `Description: ${location_obj.description}`;
+  let cloudcoverInput = document.querySelector('#Weather > :nth-child(4)');
+  cloudcoverInput.textContent = `Cloud coverage: ${location_obj.cloudcover}%`;
+  let visibilityInput = document.querySelector('#Weather > :nth-child(5)');
+  if (location_obj.visibility === 10000) {
+    location_obj.visibility = 'Clear';
+  }
+  visibilityInput.textContent = `Visibility: ${location_obj.visibility}`;
+
+  let weatherIcon = document.querySelector('#Weather >:nth-child(6)');
+  let weatherImage = document.createElement('img');
+  weatherImage.setAttribute('src', common_weather[location_obj.description]);
+  weatherImage.setAttribute('width', "100px");
+  weatherIcon.append(weatherImage);
+
+  // weather icon
+} 
 
 async function getWeather(location_obj) {
   const key = '19775201bbeb6b19e7f4e1737a8dfe1e';
@@ -116,6 +171,17 @@ async function getWeather(location_obj) {
   const jsonData2 = await response2.json();
   return jsonData2;
 }
+
+const weatherButton = document.querySelector('#weatherbutton');
+const factsButton = document.querySelector('#normalfactsbutton');
+weatherButton.addEventListener( 'click', () => {
+  document.querySelector('#Weather').style.display = "block";
+  document.querySelector('#facts').style.display = "none";
+})
+factsButton.addEventListener( 'click', () => {
+  document.querySelector('#Weather').style.display = "none";
+  document.querySelector('#facts').style.display = "block";
+})
 
 
 function parseDataIntoObject(jsonData) {
